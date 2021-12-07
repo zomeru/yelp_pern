23a8b72b8c48d2845f1870b97a542788441d5e06
Zomer <zomerzxc.19@gmail.com>
2021-12-08 06:49:52 +0800

    Added routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,59 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(express.json());
+
+// Get All Restaurants
+app.get('/api/v1/restaurants', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      restaurants: ['Restaurant 1', 'Restaurant 2', 'Restaurant 3'],
+    },
+  });
+});
+
+// Get a restaurant
+app.get('/api/v1/restaurants/:id', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      restaurant: 'Restaurant 1',
+    },
+  });
+});
+
+// Create a restaurant
+app.post('/api/v1/restaurants', (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      restaurant: 'Restaurant 1',
+    },
+  });
+});
+
+// Update a restaurant
+app.put('/api/v1/restaurants/:id', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      restaurant: 'Restaurant 1',
+    },
+  });
+});
+
+// Delete a restaurant
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 const PORT = process.env.PORT || 8000;
 
