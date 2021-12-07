bfe636fb44aaaf00af27956080505c0fe4c91759
Zomer <zomerzxc.19@gmail.com>
2021-12-08 07:26:33 +0800

    Connect postgres db

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const db = require('./db');
 const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
 // Get All Restaurants
-app.get('/api/v1/restaurants', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      restaurants: ['Restaurant 1', 'Restaurant 2', 'Restaurant 3'],
-    },
-  });
+app.get('/api/v1/restaurants', async (req, res) => {
+  try {
+    const results = await db.query('SELECT * FROM restaurants');
+
+    console.log('resulst', results);
+
+    res.status(200).json({
+      status: 'success',
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Get a restaurant
@@ -55,7 +66,7 @@ app.delete('/api/v1/restaurants/:id', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
