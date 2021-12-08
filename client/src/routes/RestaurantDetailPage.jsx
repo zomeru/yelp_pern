a910200580dd7a7a193f51d830f9e0f49d564518
Zomer <zomerzxc.19@gmail.com>
2021-12-09 05:51:18 +0800

    Added routes

---
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RestaurantDetailPage = () => {
+  return <div>Resto detail page</div>;
+};
+
+export default RestaurantDetailPage;
---
+++ client/src/routes/UpdatePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UpdatePage = () => {
+  return <div>update page</div>;
+};
+
+export default UpdatePage;
