f37cc83d0844e7038ab5c1b7051acb6a46bef822
Zomer <zomerzxc.19@gmail.com>
2021-12-09 07:41:43 +0800

    Added: Added restaurant func

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,31 +1,68 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantContext } from '../context/RestaurantContext';
 
 const AddRestaurant = () => {
+  const { addRestaurants } = useContext(RestaurantContext);
+
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [priceRange, setPriceRange] = useState('Price Range');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await RestaurantFinder.post('/', {
+        name,
+        location,
+        price_range: priceRange,
+      });
+      addRestaurants(response.data.data.restaurant);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='mb-4'>
-      <form action=''>
+      <form onSubmit={handleSubmit}>
         <div className='form-row'>
           <div className='col'>
-            <input type='text' className='form-control' placeholder='name' />
+            <input
+              type='text'
+              value={name}
+              onChange={e => setName(e.target.value)}
+              className='form-control'
+              placeholder='name'
+            />
           </div>
           <div className='col'>
             <input
+              value={location}
+              onChange={e => setLocation(e.target.value)}
               type='text'
               className='form-control'
               placeholder='location'
             />
           </div>
           <div className='col'>
-            <select className='custom-select my-1 mr-sm-2'>
+            <select
+              value={priceRange}
+              onChange={e => setPriceRange(e.target.value)}
+              className='custom-select my-1 mr-sm-2'
+            >
               <option disabled>Price Range</option>
-              <option value='1'>$1</option>
-              <option value='2'>$2</option>
-              <option value='3'>$3</option>
-              <option value='4'>$4</option>
-              <option value='5'>$5</option>
+              <option value={1}>$</option>
+              <option value={2}>$$</option>
+              <option value={3}>$$$</option>
+              <option value={4}>$$$$</option>
+              <option value={5}>$$$$$</option>
             </select>
           </div>
-          <button className='btn btn-primary'>Add</button>
+          <button type='submit' className='btn btn-primary'>
+            Add
+          </button>
         </div>
       </form>
     </div>
--- client/src/context/RestaurantContext.js
+++ client/src/context/RestaurantContext.js
@@ -5,8 +5,14 @@ export const RestaurantContext = createContext();
 export const RestaurantContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
 
+  const addRestaurants = restaurant => {
+    setRestaurants([...restaurants, restaurant]);
+  };
+
   return (
-    <RestaurantContext.Provider value={{ restaurants, setRestaurants }}>
+    <RestaurantContext.Provider
+      value={{ restaurants, setRestaurants, addRestaurants }}
+    >
       {children}
     </RestaurantContext.Provider>
   );
